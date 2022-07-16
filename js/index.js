c0c0316dfd21ffb01e5bd373dab444c9d270640f
MennatallahSherif <mennaafia22@gmail.com>
2022-07-16 22:42:17 +0200

    initial commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// let buytop=$("#sec2").offset().top
+// $(window).scroll(function()
+// {
+//     let scrollw=$(window).scrollTop()
+//     if (scrollw>buytop) {
+//         $(".nav").css("background-color","black")
+//         }
+//     else{
+//         $(".nav").css("background-color","transparent") 
+//     }
+
+// })
+  
+
